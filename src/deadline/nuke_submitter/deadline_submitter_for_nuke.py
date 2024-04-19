547cac5aabb6882f6dfde9a89f62ae48eb1aadf7
Alex Hughes <ahughesalex@gmail.com>
2024-04-19 08:22:46 -0700

    Rename environment variables containing callback hooks
    This modifies the environment variable name to include the DCC that it applies to.
    You may want a callback per-dcc so this adds flexibility
    
    Signed-off-by: Alex Hughes <ahughesalex@gmail.com>

--- src/deadline/nuke_submitter/deadline_submitter_for_nuke.py
+++ src/deadline/nuke_submitter/deadline_submitter_for_nuke.py
@@ -457,10 +457,10 @@ def show_nuke_render_submitter(parent, f=Qt.WindowFlags()) -> "SubmitJobToDeadli
         adaptor_version = ".".join(str(v) for v in adaptor_version_tuple[:2])
 
         callback_kwargs = {}
-        if os.path.exists(os.environ.get("DEADLINE_PRE_SUBMIT_CALLBACK", "")):
+        if os.path.exists(os.environ.get("DEADLINE_NUKE_PRE_SUBMIT_CALLBACK", "")):
             try:
                 on_pre_submit_callback = callback_loader.import_module_function(
-                    module_path=os.environ.get("DEADLINE_PRE_SUBMIT_CALLBACK"),
+                    module_path=os.environ.get("DEADLINE_NUKE_PRE_SUBMIT_CALLBACK"),
                     module_name="submit_callback",
                     function_name="on_pre_submit_callback",
                 )
@@ -468,7 +468,7 @@ def show_nuke_render_submitter(parent, f=Qt.WindowFlags()) -> "SubmitJobToDeadli
                 import traceback
                 raise DeadlineOperationError(
                     "Error while loading on_pre_submit_callback at {path}. {trace}".format(
-                        path=os.environ.get("DEADLINE_PRE_SUBMIT_CALLBACK"),
+                        path=os.environ.get("DEADLINE_NUKE_PRE_SUBMIT_CALLBACK"),
                         trace=traceback.format_exc()
                     )
                 )
@@ -477,16 +477,16 @@ def show_nuke_render_submitter(parent, f=Qt.WindowFlags()) -> "SubmitJobToDeadli
                 raise DeadlineOperationError(
                     "Python function at {path}:on_pre_submit_callback does not match function signature: {signature}."
                     .format(
-                        path=os.environ.get("DEADLINE_PRE_SUBMIT_CALLBACK"),
+                        path=os.environ.get("DEADLINE_NUKE_PRE_SUBMIT_CALLBACK"),
                         signature=callback_loader.CALLBACK_REFERENCE_SIGNATURE,
                     )
                 )
             callback_kwargs["on_pre_submit_callback"] = on_pre_submit_callback
 
-        if os.path.exists(os.environ.get("DEADLINE_POST_SUBMIT_CALLBACK", "")):
+        if os.path.exists(os.environ.get("DEADLINE_NUKE_POST_SUBMIT_CALLBACK", "")):
             try:
                 on_post_submit_callback = callback_loader.import_module_function(
-                    module_path=os.environ.get("DEADLINE_POST_SUBMIT_CALLBACK"),
+                    module_path=os.environ.get("DEADLINE_NUKE_POST_SUBMIT_CALLBACK"),
                     module_name="submit_callback",
                     function_name="on_post_submit_callback",
                 )
@@ -494,7 +494,7 @@ def show_nuke_render_submitter(parent, f=Qt.WindowFlags()) -> "SubmitJobToDeadli
                 import traceback
                 raise DeadlineOperationError(
                     "Error while loading on_post_submit_callback at {path}. {trace}".format(
-                        path=os.environ.get("DEADLINE_POST_SUBMIT_CALLBACK"),
+                        path=os.environ.get("DEADLINE_NUKE_POST_SUBMIT_CALLBACK"),
                         trace=traceback.format_exc()
                     )
                 )
@@ -502,7 +502,7 @@ def show_nuke_render_submitter(parent, f=Qt.WindowFlags()) -> "SubmitJobToDeadli
                 raise DeadlineOperationError(
                     "Python function at {path}:on_post_submit_callback does not match function signature: {signature}."
                     .format(
-                        path=os.environ.get("DEADLINE_POST_SUBMIT_CALLBACK"),
+                        path=os.environ.get("DEADLINE_NUKE_POST_SUBMIT_CALLBACK"),
                         signature=callback_loader.CALLBACK_REFERENCE_SIGNATURE,
                     )
                 )
